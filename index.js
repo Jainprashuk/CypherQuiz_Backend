a13ae3b72b14914cb476da517ecab000f6523741
Prashuk Jain <90614495+Jainprashuk@users.noreply.github.com>
2024-08-20 14:09:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser()); // Cookie-parser middleware should be before the routes
 app.use(cors({
-    origin: 'https://cypher-quiz-frontend.vercel.app',
+    origin: 'http://localhost:5173/',
     credentials: true,
 }));
 
