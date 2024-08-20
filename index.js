a3e0211666c004a2a53a210848af13c33c2c7ecf
Prashuk Jain <90614495+Jainprashuk@users.noreply.github.com>
2024-08-20 14:11:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser()); // Cookie-parser middleware should be before the routes
 app.use(cors({
-    origin: 'http://localhost:5173/',
+    origin: '*',
     credentials: true,
 }));
 
