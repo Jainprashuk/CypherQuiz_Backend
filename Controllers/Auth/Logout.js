30350cb0f60ea509df7b4848a4a0523395c32752
prashuk jain <29jainprashuk@gmail.com>
2024-08-20 16:04:31 +0530

    session storage logout

--- Controllers/Auth/Logout.js
+++ Controllers/Auth/Logout.js
@@ -6,10 +6,7 @@ const Logout = (req, res) => {
     console.log("i am here");
     
   // Clear the JWT token from the cookies
-  res.clearCookie('token', {
-    httpOnly: false,
-    sameSite: 'Strict'
-  });
+  sessionStorage.removeItem('token');
 
   res.status(200).json({
     message: "Logout successful",
